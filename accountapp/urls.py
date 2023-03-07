dc79f7e2cbfd92aa0b646d29118c943e49edb0eb
rhceornd <dywon93@naver.com>
2023-03-07 22:44:50 +0900

    django course 21 add create account

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,13 +1,16 @@
+# accountapp/urls.py
 
 from django.contrib import admin
 from django.urls import path
 # from . import views
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
+# 127.0.0.1:8000/account/
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world'),
+    path('hello_world/', hello_world, name='hello_world'),                  # 함수형 뷰는 이름을 넣음
+    path('create/', AccountCreateView.as_view(), name='create'), # 클래스형 뷰는 as_view() 추가
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
 # accountapp/views.py
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
-
-
-# Create your views here.
 
 def hello_world(request):
     if request.method == "POST":
@@ -21,10 +17,17 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()         # DB에 저장
 
-        # return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        return redirect('accountapp:hello_world')
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # return redirect('accountapp:hello_world')
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
+# class based view.
+# 장고에서 user 관련하여 지원해줌
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # 어느 경로로 다시 재배정    class는 reverse_lazy 사용
+    template_name = 'accountapp/create.html'
