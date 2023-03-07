4547ee59927ccff5f40b44557754da23e6996517
rhceornd <dywon93@naver.com>
2023-03-07 20:40:55 +0900

    django course 18

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 # accountapp/views.py
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -20,9 +21,10 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()         # DB에 저장
 
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        return redirect('accountapp:hello_world')
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
