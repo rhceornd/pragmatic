a3404b98071374efd091dc6d0aee81f25f564b32
rhceornd <dywon93@naver.com>
2023-03-11 16:01:48 +0900

    django course 28 decorator

---
+++ accountapp/decorators.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+
+# 내가 본 유저인지 보는 데코레이터
+def account_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+
+        user = User.objects.get(pk=kwargs['pk'])
+
+        if not (user == request.user):
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+
+    return decorated
+
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,32 +1,36 @@
 # accountapp/views.py
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [account_ownership_required, login_required]
+
+@login_required # if login, do func. Or go to login
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
 
-            temp = request.POST.get('hello_world_input')    # html에서 받은 hello_world_input을 받음
+    if request.method == "POST":
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()         # DB에 저장
+        temp = request.POST.get('hello_world_input')    # html에서 받은 hello_world_input을 받음
 
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-            # return redirect('accountapp:hello_world')
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()         # DB에 저장
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # return redirect('accountapp:hello_world')
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
 
 
 # Create view
@@ -41,6 +45,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 # Update view
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -48,32 +54,25 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 # Delete view
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+'''
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+@method_decorator(account_ownership_required, 'get')
+@method_decorator(account_ownership_required, 'post')
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+==
+has_ownership = [account_ownership_required, login_required]
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+'''
+
