77536b77e74d3f856b53f2711affe34ce4b0ae3f
rhceornd <dywon93@naver.com>
2023-03-11 16:23:13 +0900

    django course 29, setting prepare to media

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -148,3 +148,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
